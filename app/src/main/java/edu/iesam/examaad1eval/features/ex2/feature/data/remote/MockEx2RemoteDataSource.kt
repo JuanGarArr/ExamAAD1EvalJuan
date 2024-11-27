1c6ed427f6a172eb1c18a5f6546d91882e88ef0d
Juan Garcia Arruche <juan.gararr@educa.jcyl.es>
2024-11-27 15:59:52 +0100

    Start Project: Dependencies, plugins and structure

--- app/src/main/java/edu/iesam/examaad1eval/features/ex2/feature/data/remote/MockEx2RemoteDataSource.kt
+++ app/src/main/java/edu/iesam/examaad1eval/features/ex2/feature/data/remote/MockEx2RemoteDataSource.kt
@@ -1,4 +1,7 @@
-package edu.iesam.examaad1eval.features.ex2
+package edu.iesam.examaad1eval.features.ex2.feature.data.remote
+
+import edu.iesam.examaad1eval.features.ex2.feature.domain.Game
+import edu.iesam.examaad1eval.features.ex2.feature.domain.Player
 
 class MockEx2RemoteDataSource {
 
--- app/src/main/java/edu/iesam/examaad1eval/features/ex2/feature/domain/Ex2Repository.kt
+++ app/src/main/java/edu/iesam/examaad1eval/features/ex2/feature/domain/Ex2Repository.kt
@@ -1,4 +1,4 @@
-package edu.iesam.examaad1eval.features.ex2
+package edu.iesam.examaad1eval.features.ex2.feature.domain
 
 interface Ex2Repository {
     fun getGames(): List<Game>
